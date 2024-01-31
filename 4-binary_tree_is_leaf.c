ec11f351da8dd994e2638a9111e19f78f2c631e8
HAPIBANKS <posiayo1@gmail.com>
2024-01-31 14:11:21 +0100

    leaf node

--- 4-binary_tree_is_leaf.c
+++ 4-binary_tree_is_leaf.c
@@ -1,9 +1,9 @@
 #include "binary_trees.h"
 
 /**
-*
-*
-*
+*binary_tree_is_leaf - checks a node is a leaf
+*@node: pointer to the node to check
+*Return: zero
 */
 int binary_tree_is_leaf(const binary_tree_t *node)
 {
