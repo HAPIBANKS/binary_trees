6b47a1e9304f741c98ed28b888b5f66514c9377b
HAPIBANKS <posiayo1@gmail.com>
2024-01-31 14:51:03 +0100

    traversal

---
+++ 8-binary_tree_postorder.c
@@ -0,0 +1,21 @@
+#include "binary_trees.h"
+
+/**
+*binary_tree_postorder - post order traversal
+*@tree: pointer to the binary tree
+*@func: function to call for each node
+*Return: nothing
+*/
+void binary_tree_postorder(const binary_tree_t *tree, void (*func)(int))
+{
+	if (tree == NULL || func == NULL)
+	{
+		return;
+	}
+	else
+	{
+		binary_tree_preorder(tree->left, func);
+		binary_tree_preorder(tree->right, func);
+		func(tree->n);
+	}
+}
