2ed1201a3d612eccaef75c5a00248174bdfaeb76
HAPIBANKS <posiayo1@gmail.com>
2024-01-31 20:18:56 -0800

    balance factor

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -1,7 +1,7 @@
 #include "binary_trees.h"
 
 /**
-*binary_tree_height - compute the height of a tree
+*binary_tree_height_balance - compute the height of a tree
 *@tree: pointer to the root node
 *Return: height
 */
